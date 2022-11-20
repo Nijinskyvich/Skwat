6baefb188284440cb673d7817e463755fd5f4ab1
td435 <td435@cam.ac.uk>
2022-11-20 10:16:41 +0000

    Added gui

--- skwat.py
+++ skwat.py
@@ -17,7 +17,7 @@ from beatDetection import get_beats
 from audio import load_sound
 
 
-song_file = load_sound('https://www.youtube.com/watch?v=zWaymcVmJ-A')
+song_file = load_sound('https://www.youtube.com/watch?v=auAfDfZY7zI')
 
 # get song beats
 beat_times = get_beats(song_file)
@@ -40,11 +40,43 @@ rick.set_volume(0.2)
 pygame.mixer.Channel(0).play(rick)
 start_song_time = time.time()
 
+
+white = (255, 255, 255)
+green = (0, 255, 0)
+blue = (0, 0, 128)
+
+background_colour = (100,100,100)
+(width, height) = (900, 600)
+display_surface = pygame.display.set_mode((width, height))
+pygame.display.flip()
+display_surface.fill(background_colour)
+pygame.display.set_caption('Skwat Simulator 2022 - GOTY edition')
+
+
+font = pygame.font.Font('freesansbold.ttf', 32)
+text = font.render('Score :', True, green, blue)
+
+textRect = text.get_rect()
+ 
+textRect.center = (width // 2, height // 2)
+
+
+imp = pygame.image.load("SquatUp.png").convert()
+imp2 = pygame.image.load("SquatDown.png").convert()
+
+
+done = False
+running = True
+score = 0
+
+
 for i in range(100):
     line = ser.readline()
     if line:
         string = line.decode().strip()
         if string == "Squat":
+            display_surface.blit(imp2, (0,0))
+            pygame.display.update()
             squat_time_now = time.time()
 
             # find index of closest beat
@@ -74,6 +106,7 @@ for i in range(100):
                 if random.random() < 0.02:
                     pygame.mixer.Channel(1).play(pygame.mixer.Sound("goodjob.wav"))
                 print("GOOD")
+                
 
             squat_time = squat_time_now
 
@@ -104,6 +137,28 @@ for i in range(100):
                 print("YOU'RE TRASH")
             else:
                 print("GOOD")
+                
+
+    #Put voice generation bit here
+    display_surface.blit(imp, (0,0))
+
+    display_surface.blit(text, textRect)
+    pygame.display.update()
+    score+=1
+    text = font.render('Score :' + str(score), True, green, blue)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                done = True
+                running = False
+                break # break out of the for loop
+    if done:
+        break 
+
+pygame.display.quit()
+pygame.quit()
 
 ser.close()
 print("done")
